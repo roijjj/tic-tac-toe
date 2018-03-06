fbd08ac8c14975d360426f3be77183e741dfcb0d
roijjj <eddyyozombie@gmail.com>
2018-03-06 14:04:40 -0800

    Add files via upload

--- src/main/java/com/bignerdranch/android/tic_tac_toe/MainActivity.java
+++ src/main/java/com/bignerdranch/android/tic_tac_toe/MainActivity.java
@@ -1,13 +1,11 @@
 package com.bignerdranch.android.tic_tac_toe;
 
 import android.content.DialogInterface;
-import android.content.Intent;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
@@ -17,39 +15,23 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
+    //the buttions of the board
     private Button[][] mButtons = new Button[3][3];
 
+   //thie textview tells you whos turnit s
     TextView  playerturn;
 
 
     boolean p1turn = true;
 
-    String p1="player 1";
-    String p2="player 2";
+    //the player
+    String p1;
+    String p2;
 
-    int round = 0;
-
-/*
-    @Override
-    public void OnCreateOptionMenu(Menu menu, MenuInflater inflater)
-    {
-        super.onCreateOptionsMenu(menu,inflater);
-        inflater.inflate(R.menu.tic_tac_toe_menu, menu);
-        MenuItem subtitleItem = menu.findItem(R.id.menu_item_show_subtitle);
-
-    }
-@Override
-public void onCreateOptionsMenu(Menu menu, MenuInflater inflater){
-    super.onCreateOptionsMenu(menu, inflater);
-    inflater.inflate(R.menu.tic_tac_toe_menu, menu);
-    MenuItem subtitleItem = menu.findItem(R.id.menu_item_show_subtitle);
-}
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-
-    }*/
+      int round = 0;//the round you are on
 
 
+    //create the menu
     @Override
     public boolean onCreateOptionsMenu(Menu menu){
 
@@ -61,16 +43,13 @@ public void onCreateOptionsMenu(Menu menu, MenuInflater inflater){
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        /*if (item.getItemId() == R.id.editp1){
-            reset();
-
-        }*/
+//checks what the user selected in the menu
 switch (item.getItemId()){
     case R.id.reset_subtitle:
-        reset();
+        reset();//reset the board
         return true;
     case R.id.name_change:
-        showInputDialog();
+        showInputDialog();//chand the name of the players
 
         return true;
 default:
@@ -86,11 +65,13 @@ default:
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        p1= getResources().getString(R.string.player_1);
+        p2= getResources().getString(R.string.player_2);
         playerturn = findViewById(R.id.player_turn);
         changeturnview();
 
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
+        for (int i = 0; i < mButtons.length; i++) {
+            for (int j = 0; j < mButtons[i].length; j++) {
                 String bid = "button_"+ i+j;
                 int id = getResources().getIdentifier(bid,"id",getPackageName());
                 mButtons[i][j] = findViewById(id);
@@ -100,24 +81,24 @@ default:
         }
 
     }
+
     public void onClick(View v ){
 
 
+        //check if the user has pressed the button
         if (!((Button)v ).getText().toString().equals("")){
             return;
-
         }
+        //if it is the first player's turn it change the button text to x
         if (p1turn){
             ((Button)v).setText("X");
-
-
         }
         else if(!p1turn){
             ((Button)v).setText("O");
-
-
         }
 
+
+//checks if a player has won
         if (check()){
             if(p1turn){
                 win(p1);
@@ -126,23 +107,29 @@ default:
                 win(p2);
             }
         }
-        else if (round == 8){
+        else if (round == 9){
             draw();
         }
         else {
             p1turn = !p1turn;
         }
-        round++;
-        changeturnview();
+        round++;//adds 1 to the round
+        changeturnview();//changes the textview
     }
+    /*
+    checks to see if the player has won the game
+     */
     boolean check(){
 
         String[][] let = new String[3][3];
-        for (int i=0;i<3;i++){
-            for (int j=0;j<3;j++){
+
+        //put the text of button into a string 2d array
+        for (int i=0;i<mButtons.length;i++){
+            for (int j=0;j<mButtons[i].length;j++){
                 let[i][j]= mButtons[i][j].getText().toString();
             }
         }
+        //check to see if player 1 or player 2 got 3 in a row
         if ((let[0][0].equals("X") && let[0][1].equals("X") && let[0][2].equals("X"))
             ||(let[1][0].equals("X") && let[1][1].equals("X")&&let[1][2].equals("X"))
         ||(let[2][0].equals("X") && let[2][1].equals("X")&&let[2][2].equals("X"))
@@ -154,6 +141,7 @@ default:
         ){
             return true;
         }
+
         else if ((let[0][0].equals("O") && let[0][1].equals("O") && let[0][2].equals("O"))
                 ||(let[1][0].equals("O") && let[1][1].equals("O") &&let[1][2].equals("O"))
                 ||(let[2][0].equals("O") && let[2][1].equals("O")&&let[2][2].equals("O"))
@@ -170,21 +158,30 @@ default:
         }
 
     }
+    /*
+    display a message saying the player has won the game then reset the board
+     */
     void win(String name)
     {
         Toast.makeText(this, name+" has won", Toast.LENGTH_SHORT).show();
         reset();
 
     }
+    /*
+    display a message saying the players have draw the reset the board
+     */
     void draw(){
         Toast.makeText(this, "it was a draw", Toast.LENGTH_SHORT).show();
         reset();
     }
 
-
+/*
+reset the board
+ */
     void reset(){
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
+        //sets all button text to blank
+        for (int i = 0; i < mButtons.length; i++) {
+            for (int j = 0; j < mButtons[i].length; j++) {
                 mButtons[i][j].setText("");
             }
         }
@@ -215,9 +212,11 @@ default:
         p2 = SaveInstanceState.getString("p2");
 
     }
+
+
     protected void showInputDialog() {
 
-        // get prompts.xml view
+
         LayoutInflater layoutInflater = LayoutInflater.from(MainActivity.this);
         View promptView = layoutInflater.inflate(R.layout.name_diolog, null);
         AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(MainActivity.this);
@@ -226,7 +225,7 @@ default:
         final EditText editText1 = (EditText) promptView.findViewById(R.id.editp1);
         final EditText editText2 = (EditText) promptView.findViewById(R.id.editp2);
 
-        // setup a dialog window
+
         alertDialogBuilder.setCancelable(false)
                 .setPositiveButton("OK", new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
@@ -239,6 +238,7 @@ default:
                             p2 = editText2.getText().toString();
 
                         }
+                        changeturnview();
                     }
 
                 })
@@ -249,11 +249,10 @@ default:
                             }
                         });
 
-        // create an alert dialog
         AlertDialog alert = alertDialogBuilder.create();
         alert.show();
-        changeturnview();
     }
+
     public void changeturnview(){
         if(p1turn){
 
